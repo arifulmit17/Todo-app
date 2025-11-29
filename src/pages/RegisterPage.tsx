cdb34c79d10fd3596d28a55bafa2bd534c02cdeb
Ariful <arifulmit17@gmail.com>
2025-11-29 12:57:22 +0600

    implement registration form with controlled inputs and error handling

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -3,9 +3,58 @@ import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { useState } from 'react';
+import { set } from "date-fns";
 
 
 export default function RegisterPage() {
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+  // console.log(formData.email,formData.password);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name,value} = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    
+  }
+
+  const [error, setError] = useState("");
+  const handleRegister = async (e:any) => {
+    e.preventDefault();
+    
+    setError("");
+
+    try {
+      const res = await fetch("http://localhost:5000/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ formData }),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        setError(data.message || "register failed");
+        return;
+      }
+
+      // Store token
+      localStorage.setItem("token", data.token);
+
+      // Redirect user
+      window.location.href = "/dashboard";
+    } catch (err) {
+      setError("Something went wrong");
+    }
+  };
   return (
     <div className="flex justify-center items-center h-screen">
       <Card className="w-[350px]">
@@ -14,22 +63,22 @@ export default function RegisterPage() {
         </CardHeader>
 
         <CardContent>
-          <form className="space-y-4">
+          <form onSubmit={handleRegister} className="space-y-4">
             {/* Email */}
             <div className="flex flex-col space-y-1">
               <Label>Email</Label>
-              <Input type="email" placeholder="example@gmail.com" required />
+              <Input name="email" type="email" placeholder="example@gmail.com" value={formData.email} onChange={handleInputChange} required />
             </div>
 
             {/* Password */}
             <div className="flex flex-col space-y-1">
               <Label>Password</Label>
-              <Input type="password" placeholder="••••••••" required />
+              <Input name="password" type="password" value={formData.password} onChange={handleInputChange} placeholder="••••••••" required />
             </div>
 
             {/* Submit Button */}
             <Button className="w-full" type="submit">
-              Register
+              register
             </Button>
           </form>
         </CardContent>
